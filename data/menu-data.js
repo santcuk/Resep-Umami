4327edfcb81450d6c0c9cd03c29b1cba7ad88b48
santcuk <88497650+santcuk@users.noreply.github.com>
2026-02-09 01:53:23 +0700

    Add files via upload

---
+++ data/menu-data.js
@@ -0,0 +1,35 @@
+/* ===================================
+   EDIT MENU DI SINI SAJA
+   icon pakai Font Awesome class
+=================================== */
+
+const menuItems = [
+  {
+    label: "Tentang",
+    icon: "fa-circle-info",
+    link: "#"
+  },
+  {
+    label: "Hubungi Saya",
+    icon: "fa-envelope",
+    link: "#"
+  },
+  {
+    type: "divider"
+  },
+  {
+    label: "WhatsApp",
+    icon: "fa-brands fa-whatsapp",
+    link: "https://wa.me/6285362181637"
+  },
+  {
+    label: "Instagram",
+    icon: "fa-brands fa-instagram",
+    link: "https://instagram.com/dimsum_umamii"
+  },
+  {
+    label: "Telegram",
+    icon: "fa-brands fa-telegram",
+    link: "https://t.me/santoscribble"
+  }
+];
---
+++ data/resep-data.js
@@ -0,0 +1,21 @@
+/* =====================================
+   TAMBAH RESEP DI SINI SAJA
+   Format:
+   "Nama Resep" : "file.html | kategori1,kategori2"
+===================================== */
+
+const daftarResep = {
+  "Dimsum Premium": "dimpremium.html | Adonan",
+  "Dimsum Ekonomis": "dimekonomis.html | Adonan",
+  "Saus Mentai": "mentai.html | Saus,Mentai",
+  "Saus Spicy Mentai": "spicymentai.html | Saus,Mentai",
+  "Saus Cheese Mentai": "cheesementai.html | Saus,Mentai",
+  "Garlic Mayo": "garmayo.html | Saus,Mentai",
+  "Creamy Mayo": "cremayo.html | Saus,Mentai",
+  "Creamy Bolognaise": "crebolog.html | Saus,Mentai",
+  "Nashville": "nashville.html | Saus,Mentai",
+  "Cheese Nashville": "cnashville.html | Saus,Bakar",
+    "Filling Keju Lumer": "dimlumer.html | Isian",
+};
+
+
